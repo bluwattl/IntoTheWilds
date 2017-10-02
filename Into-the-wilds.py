4e5e1b09ed5bab7a722b173048d56f5285f45ee5
bluwattl <lukas@berhampore.school.nz>
2017-10-03 12:46:01 +1300

    Fixed #1
    Using wrong and operator. Also added more of crossroads.

--- Into-the-wilds.py
+++ Into-the-wilds.py
@@ -1,7 +1,6 @@
 import time
 print("INTO THE WILDS: A TEXT ADVENTURE")
 def backstory():
-    time.sleep(3)
     print("YOU LIVE IN 36TH CENTURY TOKYO, WHERE THE WORLD IS IN PANIC.")
     time.sleep(3)
     print("THERE ARE HUMAN-PANDA HYBRIDS, WHO THE NORMAL HUMANS HATE.")
@@ -32,26 +31,29 @@ rtextdisplayed = False
 alwaysCommands = ["reset", "inventory", "goals"]
 inventory = []
 #                   1                                                                                                                                2                                                                                                                  3                                                                                                                                    4
-regionalText = ["", "You are standing in your house. On your left is the door outside. In front of you is a television. Behind you is the kitchen.", "You are in a dark alleyway. There is a path, stretching right and left. Behind you is the door you came out of.", "BUGGY!! Restart game. You are in your kitchen. Behind you is the living room. There are a few knives on a rack above the counter.", "You are at a crossroads. There is an e-post on one corner. Opposite, on another corner, there is a clock."]
+regionalText = ["", "You are standing in your house. On your left is the door outside. In front of you is a television. Behind you is the kitchen.", "You are in a dark alleyway. There is a path, stretching right and left. You can also see the door to your house.", "You are in your kitchen. Behind you is the living room. There are a few knives on a rack above the counter.", "You are at a crossroads. There is an e-post on one corner. Opposite, on another corner, there is a clock."]
 while True:
     if rtextdisplayed == False:
         print(regionalText[region])
         rtextdisplayed = True
     command = input()
-    if command == "inventory":
-        print(inventory)
-    if region == 1 & rtextdisplayed == True:
+    #print("region is:" + str(region))
+    #print("command is:" + command)
+    #print("rtextdisplayed is:" + str(rtextdisplayed))
+    #print("Checking region 1 loop")
+    if region == 1 and rtextdisplayed == True:
+        #print("Got inside region 1 loop")
         if command == "walk to door":
             print("Going north...")
             time.sleep(0.5)
             region = 2
             rtextdisplayed = False
-        if command == "walk to kitchen":
+        elif command == "walk to kitchen":
             print("Going west...")
             time.sleep(0.5)
             region = 3
             rtextdisplayed = False
-        if command == "use television":
+        elif command == "use television":
             print("You turn on the televsion. There is a announcer, delivering the news.")
             time.sleep(3)
             print("Always be on the lookout for the Mutants.")
@@ -61,34 +63,48 @@ while True:
             print("Aha! So that's where I should start.")
             time.sleep(3)
             print("The news stops, to be replaced by a cartoon about a boy trying to become a 'Pokeyman Mister?' Or something like that.")
-    if region == 2 & rtextdisplayed == True:
+        else:
+            print("I didn't recognise that command!")
+    #print("Checking region 2 loop")
+    if region == 2 and rtextdisplayed == True:
+        #print("Got inside region 2 loop")
         if command == "walk to door":
             print("Going south...")
             time.sleep(0.5)
             region = 1
             rtextdisplayed = False
-        if command == "walk left":
+        elif command == "walk left":
             print("Going west...")
             time.sleep(0.5)
             region = 4
             rtextdisplayed = False
-        if command == "walk right":
+        elif command == "walk right":
             print("Going east...")
             time.sleep(0.5)
             region = 5
             rtextdisplayed = False
-        
-    if region == 3 & rtextdisplayed == True:
+        else:
+            print("I didn't recognise that command!")
+    #print("Checking region 3 loop")
+    #print("region is:" + str(region))
+    #print("command is:" + command)
+    #print("rtextdisplayed is:" + str(rtextdisplayed))
+    if region == 3 and rtextdisplayed == True:
+        #print("Got inside region 3 loop")
         if command == "pick up knife":
             print("You pick up the sharpest seeming knife.")
             time.sleep(0.5)
             inventory.extend("Knife")
-        if command == "walk to living room":
+        elif command == "walk to living room":
             print("Going east...")
             time.sleep(0.5)
             region = 1
             rtextdisplayed = False
-    if region == 4 & rtextdisplayed == True:
+        else:
+            print("I didn't recognise that command!")
+    #print("Checking region 4 loop")
+    if region == 4 and rtextdisplayed == True:
+        #print("Got inside region 4 loop")
         if command == "look at e-post":
             print("It says:")
             time.sleep(2.5)
@@ -100,11 +116,60 @@ while True:
             time.sleep(0.5)
             print("EAST: APARTMENTS.")
             time.sleep(0.5)
-        if command == "look at clock":
+        elif command == "look at clock":
             print("The time is 1:37am.")
             time.sleep(0.5)
+        elif command == "walk north":
+            print("Going north...")
+            time.sleep(0.5)
+            region = 6
+            rtextdisplayed = False
+        elif command == "walk west":
+            print("Going west...")
+            time.sleep(0.5)
+            region = 7
+            rtextdisplayed = False
+        elif command == "walk east":
+            print("Going east...")
+            time.sleep(0.5)
+            region = 2
+            rtextdisplayed = False
+        else:
+            print("I didn't recognise that command!")
+        if region == 5 and rtextdisplayed == True:
+            if command == "look at e-post":
+                print("It says:")
+                time.sleep(2.5)
+                print("E-POST: CNR HIGASHI & SHIBUYA")
+                time.sleep(0.5)
+                print("WEST: POLICE STATION, HOSPITAL, DAIGAKU HIGH SCHOOL.")
+                time.sleep(0.5)
+                print("NORTH: SHIBUYA LIBRARY, KOKUGAKUIN UNIVERSITY.")
+                time.sleep(0.5)
+                print("EAST: APARTMENTS.")
+                time.sleep(0.5)
+            elif command == "look at clock":
+                print("The time is 1:37am.")
+                time.sleep(0.5)
+            elif command == "walk north":
+                print("Going north...")
+                time.sleep(0.5)
+                region = 6
+                rtextdisplayed = False
+            elif command == "walk west":
+                print("Going west...")
+                time.sleep(0.5)
+                region = 7
+                rtextdisplayed = False
+            elif command == "walk east":
+                print("Going east...")
+                time.sleep(0.5)
+                region = 2
+                rtextdisplayed = False
+            else:
+                print("I didn't recognise that command!")
 
-        
+
 
 
 
