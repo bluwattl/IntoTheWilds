98ad0a88cf6daf7c217b2c8c3b036ad71e262e1b
bluwattl <lukas@berhampore.school.nz>
2017-12-02 08:04:33 +1300

    Added region desc

---
+++ Regions.py
@@ -0,0 +1,11 @@
+REGIONS                     |RTEXT DONE
+1=House                     |Y
+2=Alley                     |Y
+3=Kitchen                   |Y
+4=Crossroads                |Y
+5=DeadEnd                   |Y
+6=PoliceStationOutside      |Y
+7=HospitalOutside           |
+8=HighSchoolOutside         |
+9=UniversityOutside         |
+10=LibraryOutside           |
