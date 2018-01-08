19caaf1fe2f33c49125d302bf9f9babe9098816c
bluwattl <lukas@berhampore.school.nz>
2018-01-09 09:48:10 +1300

    Updated Regions

--- Regions.py
+++ Regions.py
@@ -5,7 +5,10 @@ REGIONS                     |RTEXT DONE
 4=Crossroads                |Y
 5=DeadEnd                   |Y
 6=PoliceStationOutside      |Y
-7=HospitalOutside           |
-8=HighSchoolOutside         |
-9=UniversityOutside         |
-10=LibraryOutside           |
+7=HospitalOutside           |Y
+8=HighSchoolOutside         |N
+9=UniversityOutside         |N
+10=LibraryOutside           |N
+11=MansApartment            |Y
+12=HoldingCell              |N
+13=Vents                    |N
